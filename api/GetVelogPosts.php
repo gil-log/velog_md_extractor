d8ac31f8f284943935a8741ebbd3641b2260b0e8
Gillog <swgil007@naver.com>
2023-01-19 08:01:38 +0900

    [#9] Velog Curl Receive Response

--- api/GetVelogPosts.php
+++ api/GetVelogPosts.php
@@ -5,10 +5,10 @@ require_once ('../class/RequestVelogAPI.php');
 $request = json_decode(file_get_contents("php://input"), true);
 
 try {
-    $RequestVelogAPI = new RequestVelogAPI($request['userName'], $request['cookie']);
+    $RequestVelogAPI = new RequestVelogAPI();
 } catch (Exception $e) {
     echo $e->getMessage();
     exit();
 }
 
-echo $RequestVelogAPI->testRequest();
+echo $RequestVelogAPI->curlPost("CMD-명령어로-사용중인-port-종료하기");
